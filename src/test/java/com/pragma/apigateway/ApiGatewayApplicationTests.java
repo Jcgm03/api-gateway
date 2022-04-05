19b4a7a59b480e05e59b28d6bc0d1ea1f1f9d602
Jcgm03 <100810423+Jcgm03@users.noreply.github.com>
2022-04-05 09:59:00 -0500

    Initial commit

---
+++ src/test/java/com/pragma/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
